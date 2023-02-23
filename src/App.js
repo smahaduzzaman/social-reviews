f247a069fb7fbe9354378ac0354040bdacad9aa7
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-02-23 20:33:13 +0600

    First Commit For Design Update With Socila Reviews

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
+function App() {
+  return <>
+    <Home />
+    <Login />
+    <Profile />
+    <Register />
+  </>
+  
+}
+
+export default App;
